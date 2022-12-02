07d0df5d27f10016dc8145221d06c74140ffdacd
Gopi <mittakolagopi95051@gmail.com>
2022-12-02 22:55:24 +0530

    Added copyPassword function

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var chars="";
 var passwordSize = 10;
 
-
 function genetatePassword(){
 
     var textBox = document.getElementById("box");
-    
+
     chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@";
 
     let password = "";
@@ -15,7 +14,22 @@ function genetatePassword(){
         console.log(password);
     }
     textBox.value = password;
+    document.getElementById("p1").innerHTML = "";
 }
+
+var copytext = document.getElementById("copy");
+
+function copyPassword(){
+    if(document.getElementById("box").value == ""){
+        alert("First generate password");
+    }
+    else{
+        document.getElementById("box").select();
+        document.execCommand("copy");
+        document.getElementById("p1").innerHTML = "Password Copied";
+    }
+}
+
 
         
 
